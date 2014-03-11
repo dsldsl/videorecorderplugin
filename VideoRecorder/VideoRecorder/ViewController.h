91fa7a4f09317ce8a8182c88940ce84496492c3b
Nilaf Talapady <nilaf@moonraft.ocm>
2014-03-11 22:23:48 +0530

    Added Project.

---
+++ VideoRecorder/VideoRecorder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VideoRecorder
+//
+//  Created by Nilaf Talapady on 11/03/14.
+//  Copyright (c) 2014 Nilaf Talapady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
